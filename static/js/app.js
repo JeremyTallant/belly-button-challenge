a3a259433df40258e9dcc47e82bf77d35f78ed77
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2023-01-06 11:06:55 -0600

    took away the variable when reading the json file

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,7 @@
-// Save url as a constant variable
-const samples = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-
-// Fetch the JSON data and console log it 
-d3.json(samples).then(function(data) {
-    console.log(data);
+// Fetch the JSON data and parse it
+d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(function(data) {
+  // Log the entire data object to the console
+  console.log(data);
 
 
     // Select the dropdown menu
