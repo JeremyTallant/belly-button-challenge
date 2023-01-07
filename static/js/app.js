26cab0c3b0c2ea95fdb94f677f4a4b7c4b4ba96c
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2023-01-07 12:12:53 -0600

    finished the part for the dropdown menu

--- static/js/app.js
+++ static/js/app.js
@@ -9,3 +9,31 @@ console.log("Data Promise: ", dataPromise);
 d3.json(url).then(function(data) {
   console.log(data);
 });
+
+// Initialize the dashboard at start up 
+function init() {
+
+    // Use D3 to select the dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+
+    // Set a variable for the sample names
+    let names = data.names;
+
+    // Add  samples to dropdown menu
+    names.foreach((id) => {
+        // Log the value of id for each iteration of the loop
+        console.log(id);
+        select.append("option")
+        .text(id)
+        .property("value",id);
+
+    });
+
+    // Set the first sample from the list to build the initial plots
+    let sample_one = names[0];
+    // Log the value of sample_one
+    console.log(sample_one);
+    buildMetadata(sample_one);
+    buildBarChart(sample_one);
+    buildBubbleChart(sample_one);
+}
