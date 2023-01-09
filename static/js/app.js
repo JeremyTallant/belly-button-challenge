ef5630dbcee71e19229196f0053fe69473ef10db
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2023-01-09 14:38:59 -0600

    fixed some errors

--- static/js/app.js
+++ static/js/app.js
@@ -12,30 +12,33 @@ function init() {
     // Use D3 to select the dropdown menu
     let dropdownMenu = d3.select("#selDataset");
 
-    // Set a variable for the sample names
-    let names = data.names;
+    // Use D3 to get sample names and populate the drop-down selector
+    d3.json(url).then((data) => {
+        
+        // Set a variable for the sample names
+        let names = data.names;
 
-    // Add  samples to dropdown menu
-    names.forEach((id) => {
+        // Add  samples to dropdown menu
+        names.forEach((id) => {
 
-        // Log the value of id for each iteration of the loop
-        console.log(id);
+            // Log the value of id for each iteration of the loop
+            console.log(id);
 
-        dropdownMenu.append("option")
-        .text(id)
-        .property("value",id);
+            dropdownMenu.append("option")
+            .text(id)
+            .property("value",id);
+        });
 
+        // Set the first sample from the list to build the initial plots
+        let sample_one = names[0];
+
+        // Log the value of sample_one
+        console.log(sample_one);
+
+        buildMetadata(sample_one);
+        buildBarChart(sample_one);
+        buildBubbleChart(sample_one);
     });
-
-    // Set the first sample from the list to build the initial plots
-    let sample_one = names[0];
-
-    // Log the value of sample_one
-    console.log(sample_one);
-
-    buildMetadata(sample_one);
-    buildBarChart(sample_one);
-    buildBubbleChart(sample_one);
 };
 
 // Function that populates metadata info
@@ -167,13 +170,13 @@ function buildBubbleChart(sample) {
 };
 
 // Function that updates dashboard when sample is changed
-function change(item) { 
+function OptionChanged(item) { 
 
     // Call all functions 
     buildMetadata(item);
     buildBarChart(item);
     buildBubbleChart(item);
-}
+};
 
 // Call the initialize function
 init();
