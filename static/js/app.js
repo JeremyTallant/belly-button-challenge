d30783034fc379e89c5b3dd8184c37fa824c9c38
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2023-01-09 14:26:54 -0600

    finished the bubble chart and function that handles changes

--- static/js/app.js
+++ static/js/app.js
@@ -92,7 +92,7 @@ function buildBarChart(sample) {
         let sample_values = valueData.sample_values;
 
         // Log the data to the console
-        console.log(otu_ids,otu_labels,sample_values)
+        console.log(otu_ids,otu_labels,sample_values);
 
         // Set top ten items to display in descending order
         let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
@@ -117,4 +117,64 @@ function buildBarChart(sample) {
         Plotly.newPlot("bar", [trace], layout)
     });
 };
+
+// Function that builds the bubble chart
+function buildBubbleChart(sample) {
+
+    // Use D3 to retrieve all of the data
+    d3.json(url).then((data) => {
+        
+        // Retrieve all sample data
+        let sampleInfo = data.samples;
+
+        // Filter based on the value of the sample
+        let value = sampleInfo.filter(result => result.id == sample);
+
+        // Get the first index from the array
+        let valueData = value[0];
+
+        // Get the otu_ids, lables, and sample values
+        let otu_ids = valueData.otu_ids;
+        let otu_labels = valueData.otu_labels;
+        let sample_values = valueData.sample_values;
+
+        // Log the data to the console
+        console.log(otu_ids,otu_labels,sample_values);
+        
+        // Set up the trace for bubble chart
+        let trace1 = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        };
+
+        // Set up the layout
+        let layout = {
+            title: "Bacteria Per Sample",
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"},
+        };
+
+        // Call Plotly to plot the bubble chart
+        Plotly.newPlot("bubble", [trace1], layout)
+    });
+};
+
+// Function that updates dashboard when sample is changed
+function change(item) { 
+
+    // Call all functions 
+    buildMetadata(item);
+    buildBarChart(item);
+    buildBubbleChart(item);
+}
+
+// Call the initialize function
+init();
 
