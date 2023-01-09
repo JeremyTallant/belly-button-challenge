a757cabea1fc84d0486020e5d94f056b9cd421af
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2023-01-09 12:32:24 -0600

    finished the function that populates metadata info

--- static/js/app.js
+++ static/js/app.js
@@ -37,3 +37,29 @@ function init() {
     buildBarChart(sample_one);
     buildBubbleChart(sample_one);
 }
+
+// Function that populates metadata info
+function buildMetadata(sample) {
+
+    // Use D3 to get all of the data
+    d3.json(url).then((data) => {
+
+        // Retrieve all metadata
+        let metadata = data.metadata;
+
+        // Filter based on the value of the sample
+        let value = metadata.filter(result => result.id == sample);
+
+        // Get the first index from the array
+        let valueData = value[0];
+
+        // Clear out metadata
+        d3.select("#sample-metadata").html("");
+
+        // Use Object.entries to add each key/value pair to the panel
+        Object.entries(valueData).forEach(([key,value]) => {
+            d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+        });
+    });
+
+}
