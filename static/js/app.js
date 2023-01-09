135eb2ec983d5073e67f2952d5649d8653a7b6d1
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2023-01-09 11:57:11 -0600

    fixed some errors and took out unnecessary lines

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,6 @@
 // Place url in a constant variable
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-// Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
-
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
   console.log(data);
@@ -20,10 +16,12 @@ function init() {
     let names = data.names;
 
     // Add  samples to dropdown menu
-    names.foreach((id) => {
+    names.forEach((id) => {
+
         // Log the value of id for each iteration of the loop
         console.log(id);
-        select.append("option")
+
+        dropdownMenu.append("option")
         .text(id)
         .property("value",id);
 
@@ -31,8 +29,10 @@ function init() {
 
     // Set the first sample from the list to build the initial plots
     let sample_one = names[0];
+
     // Log the value of sample_one
     console.log(sample_one);
+
     buildMetadata(sample_one);
     buildBarChart(sample_one);
     buildBubbleChart(sample_one);
