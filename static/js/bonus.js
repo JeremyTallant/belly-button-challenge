8b227622574db54be2d5d59e1fc52b0593fedec3
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2023-01-09 19:07:44 -0600

    fixing errors

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -7,38 +7,38 @@ d3.json(url).then(function(data) {
 let colorScale = d3.scaleLinear().domain([0,10]).range(["lightgreen", "darkgreen"]).interpolate(d3.interpolateHcl);
 
 // Initialize the dashboard at start up 
-//function init() {
+function init() {
 
     // Use D3 to select the dropdown menu
-    //let dropdownMenu = d3.select("#selDataset");
+    let dropdownMenu = d3.select("#selDataset");
 
     // Use D3 to get sample names and populate the drop-down selector
-    //d3.json(url).then((data) => {
+    d3.json(url).then((data) => {
         
         // Set a variable for the sample names
-        //let names = data.names;
+        let names = data.names;
 
         // Add  samples to dropdown menu
-       // names.forEach((id) => {
+        names.forEach((id) => {
 
             // Log the value of id for each iteration of the loop
-           // console.log(id);
+            console.log(id);
 
-           //dropdownMenu.append("option")
-           // .text(id)
-           // .property("value",id);
-       // });
+            dropdownMenu.append("option")
+            .text(id)
+            .property("value",id);
+        });
 
         // Set the first sample from the list
-        //let sample_one = names[0];
+        let sample_one = names[0];
 
         // Log the value of sample_one
-        //console.log(sample_one);
+        console.log(sample_one);
 
         // Build the initial plots
-       // buildGaugeChart(sample_one);
-   // });
-//};
+        buildGaugeChart(sample_one);
+    });
+};
 
 // Function that builds the gauge chart
 function buildGaugeChart(sample) {
