2420707b80cebf835b30b7cc225c1bfce8903f24
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2023-01-09 19:26:45 -0600

    changed the color of gauge chart

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -75,16 +75,16 @@ function buildGaugeChart(sample) {
                 axis: {range: [0,10], tickmode: "linear", tick0: 2, dtick: 2},
                 bar: {color: "black"},
                 steps: [
-                    {range: [0, 1], color: colorScale(0)},
-                    {range: [1, 2], color: colorScale(1)},
-                    {range: [2, 3], color: colorScale(2)},
-                    {range: [3, 4], color: colorScale(3)},
-                    {range: [4, 5], color: colorScale(4)},
-                    {range: [5, 6], color: colorScale(5)},
-                    {range: [6, 7], color: colorScale(6)},
-                    {range: [7, 8], color: colorScale(7)},
-                    {range: [8, 9], color: colorScale(8)},
-                    {range: [9, 10], color: colorScale(9)},
+                    {range: [0, 1], color: "rgba(14, 127, 0, .5)"},
+                    {range: [1, 2], color: "rgba(50, 143, 10, 0.5)"},
+                    {range: [2, 3], color: "rgba(110, 154, 22, .5)"},
+                    {range: [3, 4], color:  "rgba(142, 178, 35 , .5)"},
+                    {range: [4, 5], color:  "rgba(170, 202, 42, .5)"},
+                    {range: [5, 6], color: "rgba(184, 205, 68, .5)"},
+                    {range: [6, 7], color: "rgba(202, 209, 95, .5)"},
+                    {range: [7, 8], color:  "rgba(210, 206, 145, .5)"},
+                    {range: [8, 9], color: "rgba(232, 226, 202, .5)"},
+                    {range: [9, 10], color: "rgba(255, 255, 255, 0)"},
                 ]
             } 
         };
